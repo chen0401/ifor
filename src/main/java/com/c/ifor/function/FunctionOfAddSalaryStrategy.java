231a6ad53d726410b683beea05bc4f6406b136dd
chen0401 <chentravelling@163.com>
2021-01-22 17:50:14 +0800

    update

--- src/main/java/com/c/ifor/function/FunctionOfAddSalaryStrategy.java
+++ src/main/java/com/c/ifor/function/FunctionOfAddSalaryStrategy.java
@@ -6,15 +6,20 @@ import java.util.function.Function;
  * @author: cjianping on 2021/1/22 16:29
  */
 public class FunctionOfAddSalaryStrategy implements Function<User, Double> {
-    // 满足调薪条件
+    /**
+     * 满足调薪的条件
+     */
     private final PredicateAddSalaryCondition predicateAddSalaryCondition = new PredicateAddSalaryCondition();
+
+    private final int RUSIYEAR_01 = 5;
+    private final int RUSIYEAR_02 = 10;
 
     @Override
     public Double apply(User user) {
         if (predicateAddSalaryCondition.test(user)) {
-            if (user.getRuSiYear() <= 5) {
+            if (user.getRuSiYear() <= RUSIYEAR_01) {
                 return 0.1;
-            } else if (user.getRuSiYear() <= 10) {
+            } else if (user.getRuSiYear() <= RUSIYEAR_02) {
                 return 0.2;
             } else {
                 return 0.3;
